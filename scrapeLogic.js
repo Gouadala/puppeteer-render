5ee811187158d7438f26c830e2c94bb30b45a3ae
Gouadala <goudalastore@gmail.com>
2023-08-18 23:10:12 +0000

    screenshot

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -22,22 +22,10 @@ const scrapeLogic = async (res) => {
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
+    // Take a screenshot of the page
+    await page.screenshot({ path: "screenshot.png" });
+  
+    const logStatement = `Screenshot saved as screenshot.png`;
     console.log(logStatement);
     res.send(logStatement);
   } catch (e) {
@@ -49,5 +37,5 @@ const scrapeLogic = async (res) => {
 };
 
 module.exports = { scrapeLogic };
-// updated ciS
+
 
