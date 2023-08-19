dccd756635fb63b15f113909ab8cd4e226cdd160
Gouadala <goudalastore@gmail.com>
2023-08-19 00:07:36 +0000

    1.3(netflix)

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,7 +17,7 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://www.netflix.com/ma-en/login");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
