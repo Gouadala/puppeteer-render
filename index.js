2b003c40adaff3f9a6dc788e3e4bf67714c1d711
Gouadala <goudalastore@gmail.com>
2023-08-19 04:13:39 +0000

    checker2.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
+const { checker } = require("./checker");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -11,8 +12,14 @@ app.get("/scrape", (req, res) => {
 
 app.post("/check", async (req, res) => {
   const { email, password } = req.body;
-   const result = await checker(email, password);
-   res.json({ result });
+
+  try {
+    const result = await checker(email, password);
+    res.json({ result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while checking credentials.' });
+  }
 });
 
 app.get("/", (req, res) => {
